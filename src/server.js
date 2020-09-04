cd94d06c7c377d953f5d5e86b65520af405903c8
garuav <gbgaurav461@gmail.com>
2020-09-04 22:02:08 +0530

    added debug code for testing

--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
+    console.log('path to root directory ', __dirname);
+    
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
